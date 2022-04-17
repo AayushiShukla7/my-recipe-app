7ba804861b92777f0dda93ebad394bb34f0bb4ff
AayushiShukla7 <AayushiShukla27@gmail.com>
2022-04-17 13:04:27 +0530

    Vue JS 3 Recipe App

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
 }
-
-nav {
-  padding: 30px;
+body {
+  background-color: #030e1b;
+  color: #FFF;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a {
+  color: #2aeb74;
+  text-decoration: none;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+button {
+  appearance: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+  background-color: #2aeb74;
+  font-size: 1.125rem;
+  color: #030e1b;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My Recipes</h1>
+    <button>Add new Recipe</button>
+
+    <div class="recipes">
+      <!-- TODO: Recipes to go here! -->
+    </div>
+
+    <div class="add-recipe-popup">
+      <div class="popup-content">
+        <h2>Add new recipe</h2>
+
+        <!-- Prevent this form to get submitted -->
+        <form @submit.prevent="">
+          <div class="group">
+            <label>Title</label>
+            <input type="text" />
+          </div>
+
+          <div class="group">
+            <label>Description</label>
+            <textarea></textarea>
+          </div>
+
+          <div class="group">
+            <label>Ingredients</label>
+            <div class="ingredients">
+              <input type="text" />
+            </div>
+            <button type="button">Add Ingredient</button>
+          </div>          
+
+          <div class="group">
+            <label>Method</label>
+            <div class="method">
+              <textarea></textarea>
+            </div>
+            <button type="button">Add Step</button>
+          </div>
+
+          <button type="submit">Add Recipe</button>
+          <button type="button">Close</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style>
+.home {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
